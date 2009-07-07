032762517b25e1ea7ea0d716bae3d6ab8242a15b
cho45 <cho45@lowreal.net>
2009-07-07 14:53:49 +0900

    init

---
+++ lib/DBIx/RewriteDSN.pm
@@ -0,0 +1,33 @@
+package DBIx::RewriteDSN;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+DBIx::RewriteDSN -
+
+=head1 SYNOPSIS
+
+  use DBIx::RewriteDSN;
+
+=head1 DESCRIPTION
+
+DBIx::RewriteDSN is
+
+=head1 AUTHOR
+
+cho45 E<lt>cho45@lowreal.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
